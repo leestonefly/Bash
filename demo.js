d5c7d576f44a19739805e6e5f58230043f53aa49
Lee <787441351@qq.com>
2018-12-19 14:09:36 +0800

    js bash

---
+++ demo.js
@@ -0,0 +1,15 @@
+#!C:\Program Files\nodejs\node.exe
+var fs = require('fs')
+
+var dirName = process.argv[2] // 你传的参数是从第 2 个开始的
+try{
+    fs.mkdirSync("./" + dirName) // mkdir $1
+    fs.mkdirSync( "./" + dirName+'/css') // mkdir css
+    fs.mkdirSync("./" + dirName+'/js') // mkdir js
+    fs.writeFileSync("./"+ dirName+"/index.html", '<!DOCTYPE>\n <title>Hello</title>\n <h1>Hi</h1>')
+    fs.writeFileSync("./"+dirName+"/css/style.css", 'h1{color: red;}')
+    fs.writeFileSync("./"+dirName+"/js/main.js", 'var string = "Hello World"\nalert(string)')  
+}catch{
+    console.log("error: dir exists")
+}
+process.exit(0)
--- demo.sh
+++ demo.sh
@@ -6,7 +6,7 @@ else
   cd $1
   mkdir css js
   touch index.html css/style.css js/main.js
-  echo ' <!DOCTYPE>
+  echo '<!DOCTYPE>
   <title>Hello</title>
   <h1>Hi</h1>' >> index.html
   echo 'h1{color: red;}' >> css/style.css
